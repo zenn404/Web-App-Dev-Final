cd9359cef77b969ac26fd153d68e31c85facfa09
Chayapol Moemeng <mchayapol@gmail.com>
2025-09-10 15:10:32 +0700

    minor fix

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function BoxBasic() {
   return (
     <main>
       <Box component="section" className="border border-gray-800 m-5 text-center">
-        <h1>Stock Management v1.0</h1>
+        <h1 className="text-3xl text-violet-950">Stock Management v1.0</h1>
         <ul>
           <li><a href="/product">Products</a></li>
           <li><a href="/category">Category</a></li>
